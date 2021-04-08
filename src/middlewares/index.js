024b7aa81fcb7729c8de91fa967bc8460feb1034
cdsandoval <iznomag@gmail.com>
2021-04-08 08:37:27 +0200

    Add pagination

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,4 +2,5 @@ module.exports = {
   NotFoundMiddleware: require("./not-found.middleware"),
   ErrorMiddleware: require("./error.middleware"),
   AuthMiddleware: require("./auth.middleware"),
+  ParseIntMiddleware: require("./parse-int.middleware"),
 };
